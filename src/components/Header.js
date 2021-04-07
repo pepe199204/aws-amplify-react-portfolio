5b3fa13c2079c8f53a916f9fb4dda5f5a6b1b8ba
Felipe Mendoza Peláez <pepe199204@gmail.com>
2021-04-07 12:01:04 -0500

    change email

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = ({onAdd, showText}) => {
     return (
         <header className='header'>
             <Button id={1} text={showText ? 'About' : 'Portfolio'} onAdd={onAdd}/>
-            <Button id={2} text='Contact' link={'https://mail.google.com/mail/?view=cm&fs=1&to=pepe199204@com'}/>
+            <Button id={2} text='Contact' link={'https://mail.google.com/mail/?view=cm&fs=1&to=pepe199204@gmail.com'}/>
             <Button id={3} text='Resume' link={'/cv/cv.pdf'}/>
             <Button id={4} text='Linkedin' image='true' link={'https://www.linkedin.com/in/felipe-mendoza-pel%C3%A1ez-3285252b/'}/>
             <Button id={5} text='Github' image='true' link={'https://github.com/pepe199204'}/>
